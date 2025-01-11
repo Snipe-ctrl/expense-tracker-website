585c14b00c04cf81e6eefa14b7560cd09579a288
Snipe-ctrl <joshuarodli19@gmail.com>
2025-01-10 17:31:10 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Import the plugin
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/js/app.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: 'assets/[name][ext]',
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // Use the plugin loader
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/index.html'),
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, 'src/public/weather-icons'), to: 'weather-icons' },
+            ],
+        }),
+        new MiniCssExtractPlugin({ // Add the plugin
+            filename: '[name][contenthash].css',
+        }),
+    ],
+};
