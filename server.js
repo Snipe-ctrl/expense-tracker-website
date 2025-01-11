dc0cce3161749f5ee66ebbbd3e276045457781f0
Snipe-ctrl <joshuarodli19@gmail.com>
2025-01-11 12:24:54 -0800

    added user authentication and connected mongoDB

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const dotenv = require('dotenv');
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
 
-dotenv.config();
+const indexRouter = require('./src/js/routes/index')
+const authRouter = require('./src/js/routes/auth')
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/', indexRouter)
+app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist/index.html'));
@@ -20,6 +30,15 @@ app.use(cors({
 }));
 
 app.use(express.static(path.resolve(__dirname, 'dist')));
+
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('MongoDB connection is established successfully! 🎉')
+	})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
