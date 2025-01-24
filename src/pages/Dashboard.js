372afec1b19095ef4175b43b93eea8a9aa7d9def
Joshua Rodli <joshuarodli19@gmail.com>
2025-01-24 09:22:19 -0800

    added and styled overview cards

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-import '/src/styles/style.scss';
 import Header from "../components/Header";
+import OverviewCards from "../components/OverviewCards";
+import '/src/styles/style.scss';
 
 const Dashboard = () => {
 
     return (
         <div>
             <Header />
-            <h1>Dashboard</h1>
+            <OverviewCards/>
             <Outlet />
         </div>
     )
